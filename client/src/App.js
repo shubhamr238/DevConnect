c7e321d6ee3ab826fcb764640e785630c2ad0d26
shubhamr238 <shubhamr238@gmail.com>
2020-05-27 21:42:11 +0530

    add login and register comp, connected it through routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,21 @@ import "./App.css";
 import Navbar from "./components/layouts/navbar";
 import Footer from "./components/layouts/footer";
 import Landing from "./components/layouts/landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
