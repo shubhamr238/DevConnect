968f17329b36bd68652c099a1b6083c0b033bb71
shubhamr238 <shubhamr238@gmail.com>
2020-06-03 17:47:06 +0530

    add view single post with comments

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
 import NotFound from "./components/not-found/NotFound";
 
 import setAuthToken from "./utils/setAuthToken";
@@ -93,6 +94,9 @@ function App() {
             </Switch>
             <Switch>
               <PrivateRoute exact path="/feed" component={Posts} />
+            </Switch>
+            <Switch>
+              <PrivateRoute exact path="/post/:id" component={Post} />
             </Switch>
           </div>
           <Footer />
