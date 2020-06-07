908bcc18183deef589737902bfddb492d1e0527a
shubhamr238 <shubhamr238@gmail.com>
2020-06-07 22:36:31 +0530

    deply ready

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const passport = require("passport");
 const passportJWT = require("./config/passport-jwt-strategy");
 const app = express();
 const db = require("./config/mongoose");
+const path = require("path");
 const port = process.env.PORT || 8000;
 
 // body parser for req.body
@@ -15,6 +16,16 @@ app.use(passport.session());
 
 // use express router
 app.use("/", require("./routes"));
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //Server Listner
 app.listen(port, function (err) {
